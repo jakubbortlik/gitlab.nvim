4b9d3f56fc6e6452fbf74beab4963f105e71c09e
Álan Crístoffer <acristoffers@startmail.com>
2026-03-30 16:08:02 +0200

    fixup! feat: better handling of server installation path

--- flake.nix
+++ flake.nix
@@ -2,17 +2,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs, gomod2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        gitlab-nvim-server = pkgs.buildGoModule {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ gomod2nix.overlays.default ];
+        };
+        gitlab-nvim-server = pkgs.buildGoApplication {
           pname = "gitlab.nvim-server";
           version = "git";
           src = ./.;
-          vendorHash = "sha256-OLAKTdzqynBDHqWV5RzIpfc3xZDm6uYyLD4rxbh0DMg=";
+          modules = ./gomod2nix.toml;
+          subPackages = [ "cmd" ];
           postInstall = ''
             cp -r ${./cmd/config} $out/bin/config
             mv $out/bin/cmd $out/bin/gitlab.nvim
@@ -34,6 +42,7 @@
             git
             go
             go-tools
+            gomod2nix
             golangci-lint
             luajitPackages.busted
             luajitPackages.luacheck
